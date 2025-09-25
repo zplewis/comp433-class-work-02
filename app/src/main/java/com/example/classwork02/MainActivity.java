394240d10ac9b723e86d7808e31c53dde95b15c7
Patrick Lewis <patrick_lewis@unc.edu>
2025-09-24 23:01:30 -0400

    add remaining fixes

--- app/src/main/java/com/example/classwork02/MainActivity.java
+++ app/src/main/java/com/example/classwork02/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.classwork02;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.io.File;
+import java.io.FileOutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +26,34 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void onClickClear(View view) {
+        // The original code for getting a view by ID is:
+        // ImageView v = findViewById(R.id.iv);
+
+        // We need to use the correct class for the custom view
+        MyDrawingArea v = findViewById(R.id.drawingarea);
+
+        // Reset the path of the canvas
+        v.resetPath();
+    }
+
+
+    public void onClickSave(View view) {
+        // We need to use the correct class for the custom view
+        MyDrawingArea v = findViewById(R.id.drawingarea);
+
+        // Save the bitmap of the current image
+        Bitmap b = v.getBitmap(); //we wrote this function inside custom view
+        try {
+            File f = new File(getFilesDir().getAbsolutePath() + "/mysketch.png");
+            FileOutputStream fos = new FileOutputStream(f);
+            b.compress(Bitmap.CompressFormat.PNG, 100, fos);
+            fos.flush();
+            fos.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
